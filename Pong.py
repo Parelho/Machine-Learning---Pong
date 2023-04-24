d725691089310407a4d751838ed25aef5ca5ed2c
Parelho <vinicius.parelho@outlook.com>
2023-04-24 10:04:28 -0300

    Final changes before introducing neat

--- Pong.py
+++ Pong.py
@@ -11,10 +11,14 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (100, 100, 100)
 
-DIVISION = pygame.Rect(450, 0, 15, 600)
+DIVISION = pygame.Rect(450, 35, 15, 535)
 LEFT_RECTANGLE = pygame.Rect(25, 250, 10, 100)
 RIGHT_RECTANGLE = pygame.Rect(875, 250, 10, 100)
 BALL = pygame.Rect(450, 250, 15, 15)
+TOP_DIVISION = pygame.Rect(0, 35, 900, 15)
+BOTTOM_DIVISION = pygame.Rect(0, 565, 900, 15)
+RIGHT_DIVISION = pygame.Rect(0, 35, 15, 535)
+LEFT_DIVISION = pygame.Rect(885, 35, 15, 535)
 
 FPS = 60
 VEL = 10
@@ -33,28 +37,24 @@ def movement_left(keys_pressed, LEFT_RECTANGLE):
         else:
             LEFT_RECTANGLE.y -= VEL
 
-def movement_right(keys_pressed, RIGHT_RECTANGLE):
-    if keys_pressed[pygame.K_DOWN]:
-        if RIGHT_RECTANGLE.y > 480:
-            VEL - 10
-        else:
-            RIGHT_RECTANGLE.y += VEL
-    if keys_pressed[pygame.K_UP]:
-        if RIGHT_RECTANGLE.y < 20:
-            VEL - 10
-        else:
-            RIGHT_RECTANGLE.y -= VEL
+def movement_right(RIGHT_RECTANGLE):
+    RIGHT_RECTANGLE.y = BALL.y - 50
+
 
 
 def draw_window():
     WIN.fill(BLACK)
 
-    pygame.draw.rect(WIN, WHITE, LEFT_RECTANGLE)
-    pygame.draw.rect(WIN, WHITE, RIGHT_RECTANGLE)
     pygame.draw.rect(WIN, GREY, DIVISION)
     pygame.draw.rect(WIN, WHITE, BALL)
+    pygame.draw.rect(WIN, GREY, RIGHT_DIVISION)
+    pygame.draw.rect(WIN, GREY, LEFT_DIVISION)
+    pygame.draw.rect(WIN, GREY, TOP_DIVISION)
+    pygame.draw.rect(WIN, GREY, BOTTOM_DIVISION)
+    pygame.draw.rect(WIN, WHITE, LEFT_RECTANGLE)
+    pygame.draw.rect(WIN, WHITE, RIGHT_RECTANGLE)
 
-BALL_SPEED = 5
+BALL_SPEED = 7.5
 change_choices = (-1, 1)
 change = random.choice(change_choices)
 change_y = 1
@@ -95,9 +95,9 @@ def main():
             change = 1
         BALL.x += BALL_SPEED * change
 
-        if BALL.y < 10:
+        if BALL.y < 50:
             change_y = 1
-        if BALL.y > 590:
+        if BALL.y > 550:
             change_y = -1
         BALL.y += (BALL_SPEED/2 + 1) * change_y
 
@@ -110,7 +110,7 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         movement_left(keys_pressed, LEFT_RECTANGLE)
-        movement_right(keys_pressed, RIGHT_RECTANGLE)
+        movement_right(RIGHT_RECTANGLE)
 
         draw_window()
         show_score1(textx1, texty1)
